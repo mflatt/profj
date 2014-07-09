c0592d9a81a18c69b11229fae76d7efa832991a6
Matthew Flatt <mflatt@racket-lang.org>
2014-07-09 07:42:51 +0100

    fix dependencies

--- info.rkt
+++ info.rkt
@@ -2,4 +2,16 @@
 
 (define collection 'multi)
 
-(define deps '("combinator-parser"))
+(define deps '("combinator-parser"
+               "base"
+               "compatibility-lib"
+               "drracket-plugin-lib"
+               "errortrace-lib"
+               "gui-lib"
+               "htdp-lib"
+               "parser-tools-lib"
+               "scheme-lib"
+               "srfi-lite-lib"
+               "string-constants-lib"))
+
+(define build-deps '("scribble-lib"))
