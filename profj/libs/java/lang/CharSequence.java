11a6adf5e90637e21c7ae775efeae1bd1c43c2f9
Antonio Menezes Leitao <antonio.menezes.leitao@gmail.com>
2016-01-18 20:34:21 +0000

    Racket complains that toString is already defined.

--- profj/libs/java/lang/CharSequence.java
+++ profj/libs/java/lang/CharSequence.java
@@ -4,5 +4,5 @@ public interface CharSequence {
   public char charAt(int index);
   public int length();
   CharSequence subSequence(int start, int end);
-  String toString();
+    //AML  String toString();
 }
